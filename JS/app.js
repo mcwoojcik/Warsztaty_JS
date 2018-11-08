646492ddfdba4437a0741de10774f0baedc845e5
Kaczor <mcwoojcik@gmail.com>
2018-11-08 22:42:38 +0100

    First task of CodersLab

---
+++ JS/app.js
@@ -0,0 +1,76 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded',function(){
+
+	var matDrop = document.querySelector("li.for-dropdown");
+	var dropList = document.querySelector("div.dropdown");
+
+	matDrop.addEventListener('mouseover',function(){
+		dropList.style.display = 'block';
+	
+	});
+	matDrop.addEventListener('mouseout',function(){
+		dropList.style.display = 'none';
+	
+	});
+	
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
