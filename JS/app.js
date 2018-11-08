ca45f6653672dc0a696e32554e095fd9fe59aa89
Kaczor <mcwoojcik@gmail.com>
2018-11-08 23:30:35 +0100

    Second task from CodersLab

--- JS/app.js
+++ JS/app.js
@@ -13,8 +13,22 @@ document.addEventListener('DOMContentLoaded',function(){
 		dropList.style.display = 'none';
 	
 	});
-	
 
+
+
+	var moreButton = document.querySelectorAll('.read-more');
+	for( var i=0;i<3;i++){
+		moreButton[i].addEventListener('click',function(){
+			var textArea = this.previousElementSibling;			
+			if ( textArea.style.display === 'none' || textArea.style.display === '' ){
+				textArea.style.display = 'block';
+				this.innerHTML = 'mniej <span class="glyphicon glyphicon-chevron-up"></span>';
+			} else {
+				textArea.style.display = 'none';
+				this.innerHTML = 'mniej <span class="glyphicon glyphicon-chevron"></span>';
+			}
+		});
+	}
 });
 
 
