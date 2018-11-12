35b5290842e857394c1dc399ae6ce72e7fee039d
Kaczor <mcwoojcik@gmail.com>
2018-11-12 20:22:14 +0100

    Task 4 and 5 from coders lab

--- JS/app.js
+++ JS/app.js
@@ -31,7 +31,83 @@ document.addEventListener('DOMContentLoaded',function(){
 				this.innerHTML = 'wiecej <span class="glyphicon glyphicon-chevron-down"></span>';
 			}
 		}
+	
+	/*window.addEventListener('scroll',function(){
+		      console.log('działa przy scrollu');
+
+		var changeBottomBorder = document.querySelector('.navbar-brand');
+		changeBottomBorder.style.borderBottom = '3px solid red';	
+	});
+*/
+
+	
+/*	var menuBorder = document.querySelector('.navbar-brand');
+	window.addEventListener('scroll',function(){
+		menuBorder.style.borrderBottom = '1px solid red'});*/
+
+	//ZADANIE 4
+
+	var upButton = document.querySelector('.button1');
+		upButton.addEventListener('click',function(){
+			window.scroll(0,0);
+		});
+
+
+	//ZADANIE 5
+	var items = document.querySelectorAll('.org');
+	var dots = document.querySelectorAll('.dot');
+	
+
+	//for(var k=0;k<4;k++){
+
+		dots[0].addEventListener('click',function(){
+			for(var i=0;i<4;i++){
+				if(i!=0){
+					dots[i].classList.remove('active');
+					items[i].classList.remove('visible');
+				}
+			}
+			this.classList.add('active');
+			items[0].classList.add('visible');
+		});
+
+		dots[1].addEventListener('click',function(){
+			for(var i=0;i<4;i++){
+				if(i!=1){
+					dots[i].classList.remove('active');
+					items[i].classList.remove('visible');
+				}
+			}
+			this.classList.add('active');
+			items[1].classList.add('visible');
+		});
+		
+		dots[2].addEventListener('click',function(){
+			for(var i=0;i<4;i++){
+				if(i!=2){
+					dots[i].classList.remove('active');
+					items[i].classList.remove('visible');
+				}
+			}
+			this.classList.add('active');
+			items[2].classList.add('visible');
+		});
+	
+		dots[3].addEventListener('click',function(){
+			for(var i=0;i<4;i++){
+				if(i!=3){
+					dots[i].classList.remove('active');
+					items[i].classList.remove('visible');
+				}
+			}
+			this.classList.add('active');
+			items[3].classList.add('visible');
+		});
+	
+
 });
+
+
 
 
 
